20a7edc0ed82be1c384d1b2a18615af2fc9da894
Mr. Dariush Tasdighi <DariushT@GMail.com>
2016-10-27 17:32:30 +0330

    Init Commit

---
+++ LEARNING_CONCEPTS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LEARNING_CSHARP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack oStack = new Stack();
+
+			oStack.Push(7);
+			oStack.Push(2);
+			oStack.Push(11);
+
+			int intData;
+
+			intData = oStack.Pop();
+			System.Console.WriteLine("> " + intData);
+
+			intData = oStack.Pop();
+			System.Console.WriteLine("> " + intData);
+
+			intData = oStack.Pop();
+			System.Console.WriteLine("> " + intData);
+
+			intData = oStack.Pop();
+			System.Console.WriteLine("> " + intData);
+
+			intData = oStack.Pop();
+			System.Console.WriteLine("> " + intData);
+
+			System.Console.Write("Press [ENTER] To Exit...");
+			System.Console.ReadLine();
+		}
+	}
+}
---
+++ LEARNING_CONCEPTS/Stack.cs
@@ -0,0 +1,33 @@
+﻿namespace LEARNING_CSHARP
+{
+	public class Stack
+	{
+		public int Index = -1;
+		public System.Collections.ArrayList List = new System.Collections.ArrayList();
+
+		public void Push(int data)
+		{
+			Index++;
+
+			List.Add(data);
+		}
+
+		public int Pop()
+		{
+			if (Index >= 0)
+			{
+				int intData = (int)List[Index];
+
+				List.RemoveAt(Index);
+
+				Index--;
+
+				return (intData);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+	}
+}
